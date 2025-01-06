52694a8a63aea4acf6fbf7193ad8eb36f3749bd4
LeBronJamesGlazer <spermabobra170@gmail.com>
2025-01-06 13:08:15 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import models.Employee;
+import models.Student;
+import models.Person;
+import models.Payable;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class MyApplication {
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee("John", "Lennon", "Manager", 27045.78);
+        Employee emp2 = new Employee("George", "Harrison", "Developer", 50000.00);
+        Student stu1 = new Student("Ringo", "Starr", 2.5);  
+        Student stu2 = new Student("Paul", "McCartney", 3.8);
+
+
+        List<Person> people = new ArrayList<>();
+        people.add(emp1);
+        people.add(emp2);
+        people.add(stu1);
+        people.add(stu2);
+
+
+        Collections.sort(people);
+
+        printData(people);
+    }
+
+    public static void printData(List<Person> payables) {
+        for (Payable payable : payables) {
+            System.out.println(payable.toString() + " earns " + String.format("%.2f", payable.getPaymentAmount()) + " tenge");
         }
     }
 }
